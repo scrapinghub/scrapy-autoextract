3ec09f62351a662a5fdefe8713fa932d39af3db9
scrapinghub-ci <ivan.prado@gmail.com>
2020-12-08 11:30:22 +0000

    Fix build

--- setup.py
+++ setup.py
@@ -32,8 +32,9 @@ setup(
         #'scrapinghub-autoextract>=0.5.1',
         'scrapinghub-autoextract @ git+https://github.com/scrapinghub/scrapinghub-autoextract.git@dd109a0c9860d4ddbdc5de5c99ea53a131e1c2f2',
         #'scrapy-poet>=0.0.3',
-        'scrapy-poet @ git+https://github.com/scrapinghub/scrapy-poet.git@new-provider-interface',
-        'tldextract'
+        'scrapy-poet @ git+https://github.com/scrapinghub/scrapy-poet.git@97c315d94a2f37ce7614836853de37197f7384e1',
+        "aiohttp",
+        "tldextract",
     ],
     keywords='scrapy autoextract middleware',
     classifiers=[
