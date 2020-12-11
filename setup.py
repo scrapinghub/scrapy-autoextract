283cd1826bf74ef0330cf0b6204be45878e4e5fe
scrapinghub-ci <ivan.prado@gmail.com>
2020-12-11 16:12:20 +0000

    scrapinghub-autoextract pointing to reraise branch

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         #'autoextract-poet>=0.0.1',
         'autoextract-poet @ git+https://github.com/scrapinghub/autoextract-poet.git@modern_providers',
         #'scrapinghub-autoextract>=0.5.1',
-        'scrapinghub-autoextract @ git+https://github.com/scrapinghub/scrapinghub-autoextract.git#egg=scrapinghub-autoextract',
+        'scrapinghub-autoextract @ git+https://github.com/scrapinghub/scrapinghub-autoextract.git@reraise#egg=scrapinghub-autoextract',
         #'scrapy-poet>=0.0.3',
         'scrapy-poet @ git+https://github.com/scrapinghub/scrapy-poet.git',
         "aiohttp",
