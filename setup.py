f892d7e732e81793487d135c7bffad87c588b771
Mikhail Korobov <kmike84@gmail.com>
2020-12-30 12:59:34 +0500

    use released autoextract-poet

--- setup.py
+++ setup.py
@@ -27,8 +27,7 @@ setup(
     url='https://github.com/scrapinghub/scrapy-autoextract',
     packages=find_packages(),
     install_requires=[
-        #'autoextract-poet>=0.0.1',
-        'autoextract-poet @ git+https://github.com/scrapinghub/autoextract-poet.git@modern_providers',
+        'autoextract-poet>=0.2.0',
         'scrapinghub-autoextract>=0.6.0',
         'scrapy-poet>=0.1.0',
         'aiohttp',
