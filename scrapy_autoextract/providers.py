cf93b2e241414429e9d777cbb3470ed1edfe3273
Mikhail Korobov <kmike84@gmail.com>
2021-09-23 13:52:04 +0500

    remove AutoExtractProvider.page_type_class
    AutoExtractProvider provides all page type classes, so having
    an attribute with a single page type class is confusing.
    I'm not sure what is it for, and can't find any usages.

--- scrapy_autoextract/providers.py
+++ scrapy_autoextract/providers.py
@@ -68,8 +68,6 @@ def get_concurrent_requests_per_domain(settings: Settings):
 
 class AutoExtractProvider(PageObjectInputProvider):
     """Provider for AutoExtract data"""
-    page_type_class: ClassVar[Type]
-
     # pageType requested when only html is required
     page_type_class_for_html: ClassVar[AutoExtractData] = AutoExtractProductData
     html_query_attribute: ClassVar[str] = "fullHtml"
